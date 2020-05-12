a719165ca9bfc5e9548b4bccc836daa83c34aa0d
cj li <li@cstudio.jp>
2020-05-12 16:27:34 +0900

    modify 2

--- src/main/java/jp/cstudio/codereview/CodeReviewMain.java
+++ src/main/java/jp/cstudio/codereview/CodeReviewMain.java
@@ -2,6 +2,7 @@ package jp.cstudio.codereview;
 
 public class CodeReviewMain {
     public static void main(String args[]) {
+        System.out.println("パラメータ： " + args[0]);
         int len = Integer.parseInt(args[0]);
         char[] thing = new char[len];
 
